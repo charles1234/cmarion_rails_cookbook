0a7bf5adfd924a08f5823cee915a5ee3e2205af0
charles1234 <captainfalcon530@gmail.com>
2016-02-15 15:26:24 +0000

    added chapter 7

--- cookbook/db/seeds.rb
+++ cookbook/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create name: 'Beverages'
+Category.create name: 'Deserts'
+Category.create name: 'Appetizers'
+Category.create name: 'Entrees'
+Category.create name: 'Breakfast'
+Category.create name: 'Sandwiches'
+puts 'Created categories.'
